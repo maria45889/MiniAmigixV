851a8d5eace462d251985f6790475503c7b85c7a
Maria Jose Taco <mariajosetacoc2005@gmail.com>
2026-05-24 15:33:49 -0500

    Fix 404 for /register/ by adding alias route

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -2,12 +2,10 @@ from django.urls import path
 from .views import login_view, register_view, home_view
 
 urlpatterns = [
-
     path('', login_view, name='login'),
-
     path('registro/', register_view, name='registro'),
-
+    path('register/', register_view, name='register'),
     path('home/', home_view, name='home'),
-
 ]
+
 
