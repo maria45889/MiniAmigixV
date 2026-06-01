ed5e8c3813cf327d260dbe9ffb02f5ddd2f2af69
Maria Jose Taco <mariajosetacoc2005@gmail.com>
2026-06-01 07:31:16 -0500

    Abrir MiniAmigixV en navegador local

--- iniciar_miniamigixv.bat
+++ iniciar_miniamigixv.bat
@@ -1,9 +1,7 @@
 @echo off
 cd /d "%~dp0"
 
-if exist "env\Scripts\activate.bat" (
-    call "env\Scripts\activate.bat"
-)
+start "MiniAmigixV Server" /min cmd /k "cd /d "%~dp0" && if exist "env\Scripts\activate.bat" call "env\Scripts\activate.bat" && python manage.py runserver 127.0.0.1:8000"
 
+timeout /t 3 /nobreak > nul
 start "" "http://127.0.0.1:8000/home/"
-python manage.py runserver 127.0.0.1:8000
