38d78bf22ab9a83555c6db3b6447bcc4f4fada55
Maria Jose Taco <mariajosetacoc2005@gmail.com>
2026-08-15 14:58:51 -0500

    Fix FieldError in RegisterForm by removing password1 and password2 from Meta.fields

--- apps/app/forms/register_form.py
+++ apps/app/forms/register_form.py
@@ -20,7 +20,7 @@ class RegisterForm(UserCreationForm):
     
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email']
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
